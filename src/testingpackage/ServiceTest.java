e4ce13347e914e29aed61338cb25ba5de3fa8d69
SamHunnable <sam.hunnable@academytrainee.com>
2018-05-17 14:05:09 +0100

    Version with menu

---
+++ src/testingpackage/ServiceTest.java
@@ -0,0 +1,18 @@
+package testingpackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import accountsPackage.Service;
+
+public class ServiceTest {
+
+	@Test
+	public void testCreateAccount() {
+		Service service = new Service();
+	//	assertequals("")
+		
+	}
+
+}
